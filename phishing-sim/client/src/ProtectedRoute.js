cc5e0030dbc6a80fcc9d92acb670f8503e56243d
Rajkumar M <rajkumarmohan47@gmail.com>
2025-01-25 08:54:57 +0530

    Delete phishing-sim directory

--- phishing-sim/client/src/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children }) => {
-  const user = localStorage.getItem("user");
-  return user ? children : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
